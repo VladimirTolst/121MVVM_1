58af0fae37068c6163b5bf78cf54977dbaffbaa7
Ilya-Stafeev <stafeev-ilya@bk.ru>
2023-12-01 10:04:54 +0700

    Добавьте файлы проекта.

---
+++ ApplicationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Security.Policy;
+
+namespace _121MVVM_1
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone {Name="Денис", Surname="Батаев", Age=21 },
+                new Phone {Name="Дмитрий", Surname="Мишин", Age=18 },
+                new Phone {Name="Стас", Surname="Петухов", Age=19 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Phone.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _121MVVM_1
+{
+    public class Phone : INotifyPropertyChanged
+    {
+        private string _Name;
+        private string _Surname;
+        private int _Age;
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Surname
+        {
+            get { return _Surname; }
+            set
+            {
+                _Surname = value;
+                OnPropertyChanged("Surname");
+            }
+        }
+        public int Age
+        {
+            get { return _Age; }
+            set
+            {
+                _Age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
